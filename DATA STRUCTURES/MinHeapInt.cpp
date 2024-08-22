62f2fcd20dc5c2b7fd435171d88671a9dadab92e
Alessandro <a.pasquplay@gmail.com>
2024-08-22 11:28:02 +0200

    Separo i minHeap e maxHeap int e node

---
+++ DATA STRUCTURES/MinHeapInt.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+struct Node {
+    int data;
+    int otherData;
+
+    Node(int d, int o) : data(d), otherData(o) {}   // Solo per inizializzare in riga
+};
+
+// Comparatore per la min-heap
+struct MinHeapComparatorNode {
+    bool operator()(Node& a, Node& b) const{
+        return a.data > b.data; // Min-heap: il valore di data più piccolo ha la priorità più alta
+    }
+};
+
+
+int main(int argc, char const *argv[])
+{
+
+    priority_queue<Node, vector<Node>, MinHeapComparatorNode> minHeapNode;
+
+    // Input
+    minHeapNode.push(Node(10, 4));
+    minHeapNode.push(Node(5, 6));
+    minHeapNode.push(Node(15, 8));
+    minHeapNode.push(Node(2, 23));
+
+    // Output
+    while (!minHeapNode.empty()) {
+        cout << minHeapNode.top().data << " ";
+        minHeapNode.pop();
+    }
+
+    return 0;
+}
