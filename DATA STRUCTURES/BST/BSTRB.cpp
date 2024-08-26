095ea356b101ab34010d5b45d903ff6f270bdd5f
Alessandro <a.pasquplay@gmail.com>
2024-08-26 02:05:30 +0200

    Finita BSTRB

--- DATA STRUCTURES/BST/BSTRB.cpp
+++ DATA STRUCTURES/BST/BSTRB.cpp
@@ -1,124 +1,122 @@
 #include <iostream>
-#include <iomanip>
 
 using namespace std;
 
-// Struttura per rappresentare un nodo nell'albero rosso-nero
 struct Node {
     int key;
-    bool color;
+    bool color; // true per rosso, false per nero
     Node *left, *right, *parent;
 
-    Node(int key) : key(key), color(1), left(nullptr), right(nullptr), parent(nullptr) {}  // Costruttore
+    Node(int key) : key(key), color(true), left(nullptr), right(nullptr), parent(nullptr) {}  // Costruttore
 };
 
-// Classe per rappresentare l'albero rosso-nero
 class RedBlackTree {
-    private:
-        Node* root;  // Radice dell'albero
+private:
+    Node* root; // Radice dell'albero
+    Node* NIL;  // Nodo foglia
 
-        void LeftRotate(Node*&);
-        void RightRotate(Node*&);
-        void FixInsertViolation(Node*&);
-        void FixDeleteViolation(Node*&);
-        void Transplant(Node*, Node*);
-        Node* MinimumByNode(Node*);
-        Node* MaximumByNode(Node*);
+    void LeftRotate(Node* x);
+    void RightRotate(Node* y);
+    void FixInsertViolation(Node* z);
+    void FixDeleteViolation(Node* x);
+    void Transplant(Node* u, Node* v);
+    Node* MinimumByNode(Node* node);
+    Node* MaximumByNode(Node* node);
 
-    public:
-        RedBlackTree();
-        Node* Minimum();
-        Node* Maximum();
-        Node* Predecessor(Node*);
-        Node* Successor(Node*);
-        void Insert(int);
-        void DeleteNode(int);
-        Node* Search(int);
-        void PrintTreeHelper(Node*, int);
-        void PrintTree();
+public:
+    RedBlackTree();
+    Node* Minimum();
+    Node* Maximum();
+    Node* Predecessor(Node* x);
+    Node* Successor(Node* x);
+    void Insert(int key);
+    void Delete(int key);
+    Node* Search(int key);
+    void PrintTreeHelper(Node* node, int indent);
+    void PrintTree();
 };
 
-// Costruttore
-RedBlackTree::RedBlackTree(){
-    root = nullptr;
+// Costruttore della classe RedBlackTree
+RedBlackTree::RedBlackTree() {
+    NIL = new Node(0);
+    NIL->color = false;
+    root = NIL;
 }
 
-// Rotazione a sinistra
-void RedBlackTree::LeftRotate(Node*& pt) {
-    Node* pt_right = pt->right;
+// Funzione di rotazione a sinistra
+void RedBlackTree::LeftRotate(Node* x) {
 
-    pt->right = pt_right->left;
-    if (pt->right != nullptr)
-        pt->right->parent = pt;
-
-    pt_right->parent = pt->parent;
-
-    if (pt->parent == nullptr)
-        root = pt_right;
-    else if (pt == pt->parent->left)
-        pt->parent->left = pt_right;
-    else
-        pt->parent->right = pt_right;
-
-    pt_right->left = pt;
-    pt->parent = pt_right;
+    Node* y = x->right;
+    x->right = y->left;
+    if (y->left != NIL) {
+        y->left->parent = x;
+    }
+    y->parent = x->parent;
+    if (x->parent == NIL) {
+        root = y;
+    } else if (x == x->parent->left) {
+        x->parent->left = y;
+    } else {
+        x->parent->right = y;
+    }
+    y->left = x;
+    x->parent = y;
 }
 
-// Rotazione a destra
-void RedBlackTree::RightRotate(Node*& pt) {
-    Node* pt_left = pt->left;
+// Funzione di rotazione a destra
+void RedBlackTree::RightRotate(Node* y) {
 
-    pt->left = pt_left->right;
-    if (pt->left != nullptr)
-        pt->left->parent = pt;
-
-    pt_left->parent = pt->parent;
-
-    if (pt->parent == nullptr)
-        root = pt_left;
-    else if (pt == pt->parent->left)
-        pt->parent->left = pt_left;
-    else
-        pt->parent->right = pt_left;
-
-    pt_left->right = pt;
-    pt->parent = pt_left;
+    Node* x = y->left;
+    y->left = x->right;
+    if (x->right != NIL) {
+        x->right->parent = y;
+    }
+    x->parent = y->parent;
+    if (y->parent == NIL) {
+        root = x;
+    } else if (y == y->parent->left) {
+        y->parent->left = x;
+    } else {
+        y->parent->right = x;
+    }
+    x->right = y;
+    y->parent = x;
 }
 
 // Corregge le violazioni delle proprietà dell'albero rosso-nero dopo l'inserimento
-void RedBlackTree::FixInsertViolation(Node*& pt) {
-    while (pt->parent->color) { // Se il genitore è rosso
-        if (pt->parent == pt->parent->parent->left) {
-            Node *y = pt->parent->parent->right;
+void RedBlackTree::FixInsertViolation(Node* z) {
+    while (z->parent->color) { // Se il genitore è rosso
+        if (z->parent == z->parent->parent->left) {
+            Node *y = z->parent->parent->right;
             if (y->color) { // Se lo zio è rosso
-                pt->parent->color = false; // Genitore diventa nero
+                z->parent->color = false; // Genitore diventa nero
                 y->color = false; // Zio diventa nero
-                pt->parent->parent->color = true; // Nonno diventa rosso
-                pt = pt->parent->parent;
+                z->parent->parent->color = true; // Nonno diventa rosso
+                z = z->parent->parent;
             } else {
-                if (pt == pt->parent->right) {
-                    pt = pt->parent;
-                    LeftRotate(pt);
+                if (z == z->parent->right) {
+                    z = z->parent;
+                    LeftRotate(z);
                 }
-                pt->parent->color = false;
-                pt->parent->parent->color = true;
-                RightRotate(pt->parent->parent);
+                z->parent->color = false;
+                z->parent->parent->color = true;
+                RightRotate(z->parent->parent);
             }
         } else {
-            Node *y = pt->parent->parent->left;
+            Node *y = z->parent->parent->left;
             if (y->color) {
-                pt->parent->color = false;
+                z->parent->color = false;
                 y->color = false;
-                pt->parent->parent->color = true;
-                pt = pt->parent->parent;
+                z->parent->parent->color = true;
+                z = z->parent->parent;
             } else {
-                if (pt == pt->parent->left) {
-                    pt = pt->parent;
-                    RightRotate(pt);
+                if (z == z->parent->left) {
+                    z = z->parent;
+                    RightRotate(z);
                 }
-                pt->parent->color = false;
-                pt->parent->parent->color = true;
-                LeftRotate(pt->parent->parent);
+                z->parent->color = false;
+                z->parent->parent->color = true;
+                LeftRotate(z->parent->parent);
             }
         }
     }
@@ -126,51 +124,52 @@ void RedBlackTree::FixInsertViolation(Node*& pt) {
 }
 
 // Corregge le violazioni delle proprietà dell'albero rosso-nero dopo la cancellazione
-void RedBlackTree::FixDeleteViolation(Node*& x) {
-    while (x != root && !x->color) {
+void RedBlackTree::FixDeleteViolation(Node* x) {
+    Node* w;
+    while (x != root && x->color == false) {
         if (x == x->parent->left) {
-            Node *w = x->parent->right;
-            if (w->color) {
+            w = x->parent->right;
+            if (w->color == true) { // Caso 1
                 w->color = false;
                 x->parent->color = true;
                 LeftRotate(x->parent);
                 w = x->parent->right;
             }
-            if (!w->left->color && !w->right->color) {
+            if (w->left->color == false && w->right->color == false) { // Caso 2
                 w->color = true;
                 x = x->parent;
             } else {
-                if (!w->right->color) {
+                if (w->right->color == false) { // Caso 3
                     w->left->color = false;
                     w->color = true;
                     RightRotate(w);
                     w = x->parent->right;
                 }
-                w->color = x->parent->color;
+                w->color = x->parent->color; // Caso 4
                 x->parent->color = false;
                 w->right->color = false;
                 LeftRotate(x->parent);
                 x = root;
             }
         } else {
-            Node *w = x->parent->left;
-            if (w->color) {
+            w = x->parent->left;
+            if (w->color == true) { // Caso 1
                 w->color = false;
                 x->parent->color = true;
                 RightRotate(x->parent);
                 w = x->parent->left;
             }
-            if (!w->right->color && !w->left->color) {
+            if (w->right->color == false && w->right->color == false) { // Caso 2
                 w->color = true;
                 x = x->parent;
             } else {
-                if (!w->left->color) {
+                if (w->left->color == false) { // Caso 3
                     w->right->color = false;
                     w->color = true;
                     LeftRotate(w);
                     w = x->parent->left;
                 }
-                w->color = x->parent->color;
+                w->color = x->parent->color; // Caso 4
                 x->parent->color = false;
                 w->left->color = false;
                 RightRotate(x->parent);
@@ -181,89 +180,64 @@ void RedBlackTree::FixDeleteViolation(Node*& x) {
     x->color = false;
 }
 
-// Sostituisce un sottoalbero con un altro
+// Sostituisce il nodo u con il nodo v
 void RedBlackTree::Transplant(Node* u, Node* v) {
-    if (u->parent == nullptr)
+    if (u->parent == NIL) {
         root = v;
-    else if (u == u->parent->left)
+    } else if (u == u->parent->left) {
         u->parent->left = v;
-    else
+    } else {
         u->parent->right = v;
-
-    if (v != nullptr)
-        v->parent = u->parent;
-}
-
-// Nodo minimo a partire da un nodo
-Node* RedBlackTree::MinimumByNode(Node* node){
-
-    while(node->left != nullptr)
-        node = node->left;
-
-    return node;
-}
-
-// Nodo massimo a partire da un nodo
-Node* RedBlackTree::MaximumByNode(Node* node){
-
-    while(node->right != nullptr)
-        node = node->right;
-
-    return node;
-}
-
-// Nodo minimo
-Node* RedBlackTree::Minimum(){
-    Node* node = root;
-
-    while(node->left != nullptr)
-        node = node->left;
-
-    return node;
-}
-
-// Nodo massimo
-Node* RedBlackTree::Maximum(){
-    Node* node = root;
-
-    while(node->right != nullptr)
-        node = node->right;
-
-    return node;
-}
-
-Node* RedBlackTree::Predecessor(Node* x){
-    if (x == nullptr) {
-        return nullptr; // Se il nodo è la sentinella, restituisci la sentinella stessa
     }
+    v->parent = u->parent;
+}
 
-    // Se il nodo ha un sottoalbero sinistro, il predecessore è il massimo di quel sottoalbero
-    if (x->left != nullptr) {
+// Trova il nodo minimo a partire dal nodo dato (serve nel delete)
+Node* RedBlackTree::MinimumByNode(Node* node) {
+    while (node->left != NIL) {
+        node = node->left;
+    }
+    return node;
+}
+
+// Trova il nodo massimo a partire dal nodo dato
+Node* RedBlackTree::MaximumByNode(Node* node) {
+    while (node->right != NIL) {
+        node = node->right;
+    }
+    return node;
+}
+
+// Restituisce il nodo minimo dell'albero
+Node* RedBlackTree::Minimum() {
+    return MinimumByNode(root);
+}
+
+// Restituisce il nodo massimo dell'albero
+Node* RedBlackTree::Maximum() {
+    return MaximumByNode(root);
+}
+
+// Trova il predecessore del nodo dato
+Node* RedBlackTree::Predecessor(Node* x) {
+    if (x->left != NIL) {
         return MaximumByNode(x->left);
     }
-
-    // Se non ha sottoalbero sinistro, bisogna risalire l'albero per trovare il predecessore
-    Node *y = x->parent;
-    while (y != nullptr && x == y->left) {
+    Node* y = x->parent;
+    while (y != NIL && x == y->left) {
         x = y;
         y = y->parent;
     }
     return y;
 }
 
+// Trova il successore del nodo dato
 Node* RedBlackTree::Successor(Node* x) {
-    if (x == nullptr) {
-        return nullptr; // Se il nodo è null, restituisci null
-    }
-
-    // Caso 1: Se il nodo ha un sottoalbero destro, il successore è il minimo di quel sottoalbero
-    if (x->right != nullptr) {
+    if (x->right != NIL) {
         return MinimumByNode(x->right);
     }
-
-    // Caso 2: Se non ha sottoalbero destro, risalire l'albero per trovare il successore
     Node* y = x->parent;
-    while (y != nullptr && x == y->right) {
+    while (y != NIL && x == y->right) {
         x = y;
         y = y->parent;
     }
@@ -272,132 +246,137 @@ Node* RedBlackTree::Successor(Node* x) {
 
 // Inserisce un nuovo nodo nell'albero
 void RedBlackTree::Insert(int key) {
-    Node* pt = new Node(key);
+    Node* newNodo = new Node(key);
+    Node* parentNewNodo = NIL;
+    Node* posNewNodo = root;
 
-    // Inserimento del nodo utilizzando il classico inserimento di un BST
-    if (root == nullptr) {
-        root = pt;
-        pt->color = 0;
-        return;
+    while (posNewNodo != NIL) {
+        parentNewNodo = posNewNodo;
+        if (newNodo->key < posNewNodo->key) {
+            posNewNodo = posNewNodo->left;
+        } else {
+            posNewNodo = posNewNodo->right;
+        }
     }
 
-    Node* parent = nullptr;
-    Node* current = root;
-
-    while (current != nullptr) {
-        parent = current;
-        if (pt->key < current->key)
-            current = current->left;
-        else
-            current = current->right;
+    newNodo->parent = parentNewNodo;
+    if (parentNewNodo == NIL) {
+        root = newNodo;
+    } else if (newNodo->key < parentNewNodo->key) {
+        parentNewNodo->left = newNodo;
+    } else {
+        parentNewNodo->right = newNodo;
     }
 
-    pt->parent = parent;
+    newNodo->left = NIL;
+    newNodo->right = NIL;
+    newNodo->color = true;
 
-    if (pt->key < parent->key)
-        parent->left = pt;
-    else
-        parent->right = pt;
-
-    FixInsertViolation(pt);
+    FixInsertViolation(newNodo);
 }
 
-// Rimuove un nodo dall'albero
-void RedBlackTree::DeleteNode(int key) {
-
+// Elimina un nodo con una chiave specifica
+void RedBlackTree::Delete(int key) {
     Node* z = Search(key);
-    if (z == nullptr) return;
 
     Node* y = z;
     Node* x;
-    bool y_original_color = y->color;
+    bool yOriginalColor = y->color;
 
-    if (z->left == nullptr) {
+    if (z->left == NIL) {
         x = z->right;
         Transplant(z, z->right);
-    } else if (z->right == nullptr) {
+    } else if (z->right == NIL) {
         x = z->left;
         Transplant(z, z->left);
     } else {
         y = MinimumByNode(z->right);
-        y_original_color = y->color;
+        yOriginalColor = y->color;
         x = y->right;
-        if (y->parent == z)
+        if (y->parent == z) {
             x->parent = y;
-        else {
+        } else {
             Transplant(y, y->right);
             y->right = z->right;
             y->right->parent = y;
         }
-
         Transplant(z, y);
         y->left = z->left;
         y->left->parent = y;
         y->color = z->color;
     }
 
-    if (y_original_color == 0)
+    if (yOriginalColor == false) {
         FixDeleteViolation(x);
-
+    }
     delete z;
 }
 
+// Cerca un nodo con una chiave specifica
 Node* RedBlackTree::Search(int key) {
-    Node* current = root;  // Inizia dalla radice
-
-    while (current != nullptr) {
-        if (key == current->key) {
-            return current;  // La chiave è stata trovata
-        } else if (key < current->key) {
-            current = current->left;  // Vai al sottoalbero sinistro
+    Node* x = root;
+    while (x != NIL && key != x->key) {
+        if (key < x->key) {
+            x = x->left;
         } else {
-            current = current->right;  // Vai al sottoalbero destro
+            x = x->right;
         }
     }
-
-    return nullptr;  // La chiave non è stata trovata
+    return x;
 }
 
-// Mostra la struttura dell'albero
-void RedBlackTree::PrintTreeHelper(Node* node, int indent){
-    if (node != nullptr) {
-        // Stampa il sottoalbero destro con indentazione
-        if (node->right != nullptr) {
-            PrintTreeHelper(node->right, indent + 4);
-        }
-        
-        // Stampa il nodo corrente
-        cout << setw(indent) << ' ' << node->key << (node->color == 1 ? " (R)" : " (B)") << endl;
 
-        // Stampa il sottoalbero sinistro con indentazione
-        if (node->left != nullptr) {
-            PrintTreeHelper(node->left, indent + 4);
-        }
+#include <iomanip>
+// Funzione per stampare l'albero rosso-nero con un'indentazione
+void RedBlackTree::PrintTreeHelper(Node* node, int indent) {
+    
+    if (node == NIL) {
+        return; // Non fare nulla se il nodo è NIL
+    }
+
+    // Stampa il sottoalbero destro
+    if (node->right != NIL) {
+        PrintTreeHelper(node->right, indent + 4);
+    }
+
+    // Stampa il nodo corrente
+    if (indent > 0) {
+        std::cout << std::setw(indent) << ' ';
+    }
+    std::cout << (node->color ? "R" : "B") << node->key << "\n";
+
+    // Stampa il sottoalbero sinistro
+    if (node->left != NIL) {
+        PrintTreeHelper(node->left, indent + 4);
     }
 }
 
-void RedBlackTree::PrintTree(){
+// Stampa l'intero albero
+void RedBlackTree::PrintTree() {
+    cout<<endl<<endl;
     PrintTreeHelper(root, 0);
 }
 
-
 int main() {
-    RedBlackTree tree;
 
+
+    RedBlackTree tree;
     tree.Insert(10);
     tree.Insert(20);
+
+    tree.PrintTree();
+
     tree.Insert(30);
-    cout<<"p";
+
+    tree.PrintTree();
+
     tree.Insert(15);
-    tree.Insert(25);
-    tree.Insert(5);
 
     tree.PrintTree();
 
-    tree.DeleteNode(10);
+    tree.Delete(20);
 
     tree.PrintTree();
-
 
     return 0;
 }
